dffe4bd777a265654f2aed6e7387f24a69bb6cfb
Matej <dobrakmato@gmail.com>
2018-05-27 09:59:14 +0200

    updated to latest Facebook file structure and fixed FB encoding issues
    closes issues #2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import datetime
 import json
 import os
-from time import time
+from json import JSONDecodeError
 from os import path
 from sys import argv
-
+from time import time
 from typing import List
 
 import statistics
@@ -83,23 +83,45 @@ class FacebookStatistics:
             named_conversation = self.parse_conversation(file)
             i += 1
 
+            if named_conversation is None:
+                continue
+
             # Exclude conversation with self and group conversations if setting is enabled
             if not self.exclude_group_chats or len(named_conversation[1]) == 1:
                 self.conversations.append(named_conversation)
 
         print(f'Parsed {i-1} conversations in {time() - time_start} seconds.')
 
-    def parse_conversation(self, thread_dir: str) -> NamedConversation:
+    def parse_conversation(self, thread_dir: str) -> NamedConversation | None:
         """
-        Parses conversation from JSON file specified by thread_dir parameter and returns 
+        Parses conversation from JSON file specified by thread_dir parameter and returns
         its participants, title and messages.
-        
-        :param thread_dir: directory to parse conversation from (AdamSulko_3c954401d0 for example) 
+
+        :param thread_dir: directory to parse conversation from (AdamSulko_3c954401d0 for example)
         :return: parsed conversation
         """
+        with open(path.join(self.root_path, 'messages', thread_dir, 'message.json'),
+                  encoding='raw_unicode_escape') as f:
 
-        doc = json.load(
-            open(path.join(self.root_path, 'messages', thread_dir, 'message.json'), encoding=self.encoding))
+            # Facebook export tool produces invalid JSONs. Here we try to fix
+            # wrongly encoded characters.
+            encoded = f.read().encode('raw_unicode_escape')
+            decoded = encoded.decode()
+
+            # Also some of the control characters are not encoded correctly, so
+            # we remove all of them - we are safe to remove line break as JSON is
+            # valid without any whitespace.
+            for i in range(32):
+                decoded = decoded.replace(chr(i), '')
+
+            try:
+                doc = json.loads(decoded)
+            except JSONDecodeError as e:
+                print(">>>>> JSON DECODE ERROR")
+                print(e)
+                with open('error_file.json', mode='w', encoding='utf-8') as g:
+                    g.write(decoded)
+                return None
 
         messages = []
         participants = doc.get('participants', set())
@@ -125,7 +147,7 @@ class FacebookStatistics:
         """
         Runs all statistics for specified list of conversations.
         :param conversations: list of conversation to run statistics generators on
-        :return: 
+        :return:
         """
         for func in [self.global_stats, self.top_conversations_by_chars, self.top_conversations_by_messages,
                      self.conversation_people_variability, self.hourly_histogram, self.years_histogram,
